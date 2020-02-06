2eab4f17f3baf49f0c3f1e9d511133a8e707c8bf
wantedHorizon <eliran.amzalag@gmail.com>
2020-02-06 21:39:39 +0200

    Updates

---
+++ precache-manifest.d7edfb52ea49dc7f20bca1cb51f86fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a5c8633c3089356cdffa813436c89c",
+    "url": "/checkers-online-game/index.html"
+  },
+  {
+    "revision": "2709856b1d8648114b58",
+    "url": "/checkers-online-game/static/css/main.d7ccce98.chunk.css"
+  },
+  {
+    "revision": "0509e03f7107183ba70f",
+    "url": "/checkers-online-game/static/js/2.e94072b2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/checkers-online-game/static/js/2.e94072b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2709856b1d8648114b58",
+    "url": "/checkers-online-game/static/js/main.f55027d7.chunk.js"
+  },
+  {
+    "revision": "3081d0a85ef477283d3a",
+    "url": "/checkers-online-game/static/js/runtime-main.9aee185a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/checkers-online-game/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/checkers-online-game/precache-manifest.d7edfb52ea49dc7f20bca1cb51f86fe3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/checkers-online-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
